b0674652e558ee4c34b72a65ff4123fff4a37ae5
cyubahiro Richard <cyubahirorichard250@gmail.com>
2025-01-18 12:39:57 -0800

    CRUD

--- index.js
+++ index.js
@@ -31,9 +31,11 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
-app.get('/api/products/:id', (req, res) => {
+app.get('/api/product/:id', async (req, res) => {
   try {
-
+    const { id } = req.params;
+      const product = await Product.findById(id);
+      res.status(200).json(product)
   } catch(error) {
      res.status(500).json({message: error.message});
   }
@@ -48,7 +50,45 @@ app.post('/api/products', async (req, res) => {
   }
 })
 
-// Start the server
+// update a product
+
+app.put('/api/product/:id', async (req, res) => {
+  try {
+
+    const { id } = req.params;
+
+   const product = await Product.findByIdAndUpdate(id, req.body)
+   
+   if(!product) {
+        return res.status(404).json({message: "Product not found!"});
+   };
+
+   const updatedProduct = await Product.findById(id);
+   res.status(200).json(updatedProduct);
+
+  } catch(error) {
+      res.status(500).json({message: error.message});
+  }
+});
+
+// delet product
+
+app.delete('/api/product/:id', async (req, res) => {
+     try {
+      const { id } = req.params;
+
+      const product = await Product.findByIdAndDelete(id);
+
+      if(!product) {
+        return res.status(404).json({message: "product not found!"})
+      }
+
+      res.status(200).json({message: "message deleted successfully!"});
+
+     } catch(error) {
+        res.status(500).json({message: error.message})
+     }
+});
 
 
 
