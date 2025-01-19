b0cd54c78f92f2fe492cf3ab806c42204d4753df
cyubahiro Richard <cyubahirorichard250@gmail.com>
2025-01-18 21:13:11 -0800

    adding routers

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const { readFile } = require('fs').promises;
 const mongoose = require('mongoose');
 const Product  = require('./models/product.model.js');
+const productsRoute = require('./routes/product.route.js')
 const { error } = require('console');
 const app = express();
 
+// middle ware
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use('/api/products', productsRoute);
 
 
 const port = process.env.PORT || 3000;
@@ -21,74 +27,74 @@ app.get('/', async (req, res) => {
   }
 });
 
-app.get('/api/products', async (req, res) => {
-  try {
-     const products = await Product.find({});
-     res.status(200).json(products)
-  } catch(error) {
+// app.get('/api/products', async (req, res) => {
+//   try {
+//      const products = await Product.find({});
+//      res.status(200).json(products)
+//   } catch(error) {
       
-    res.status(500).json({message: error.message});
-  }
-});
+//     res.status(500).json({message: error.message});
+//   }
+// });
 
-app.get('/api/product/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-      const product = await Product.findById(id);
-      res.status(200).json(product)
-  } catch(error) {
-     res.status(500).json({message: error.message});
-  }
-});
+// app.get('/api/products/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//       const product = await Product.findById(id);
+//       res.status(200).json(product)
+//   } catch(error) {
+//      res.status(500).json({message: error.message});
+//   }
+// });
 
-app.post('/api/products', async (req, res) => {
-  try{
-   const product = await Product.create(req.body);
-   res.status(200).json(product)
-  } catch (error) {
-      res.status(500).json({message: error.message})
-  }
-})
+// app.post('/api/products', async (req, res) => {
+//   try{
+//    const product = await Product.create(req.body);
+//    res.status(200).json(product)
+//   } catch (error) {
+//       res.status(500).json({message: error.message})
+//   }
+// })
 
 // update a product
 
-app.put('/api/product/:id', async (req, res) => {
-  try {
+// app.put('/api/products/:id', async (req, res) => {
+//   try {
 
-    const { id } = req.params;
+//     const { id } = req.params;
 
-   const product = await Product.findByIdAndUpdate(id, req.body)
+//    const product = await Product.findByIdAndUpdate(id, req.body)
    
-   if(!product) {
-        return res.status(404).json({message: "Product not found!"});
-   };
+//    if(!product) {
+//         return res.status(404).json({message: "Product not found!"});
+//    };
 
-   const updatedProduct = await Product.findById(id);
-   res.status(200).json(updatedProduct);
+//    const updatedProduct = await Product.findById(id);
+//    res.status(200).json(updatedProduct);
 
-  } catch(error) {
-      res.status(500).json({message: error.message});
-  }
-});
+//   } catch(error) {
+//       res.status(500).json({message: error.message});
+//   }
+// });
 
 // delet product
 
-app.delete('/api/product/:id', async (req, res) => {
-     try {
-      const { id } = req.params;
+// app.delete('/api/products/:id', async (req, res) => {
+//      try {
+//       const { id } = req.params;
 
-      const product = await Product.findByIdAndDelete(id);
+//       const product = await Product.findByIdAndDelete(id);
 
-      if(!product) {
-        return res.status(404).json({message: "product not found!"})
-      }
+//       if(!product) {
+//         return res.status(404).json({message: "product not found!"})
+//       }
 
-      res.status(200).json({message: "message deleted successfully!"});
+//       res.status(200).json({message: "message deleted successfully!"});
 
-     } catch(error) {
-        res.status(500).json({message: error.message})
-     }
-});
+//      } catch(error) {
+//         res.status(500).json({message: error.message})
+//      }
+// });
 
 
 
